0ac581c5f222ee2c87c4d9ac1320d20bf180fad0
samirkhaled <samir.k33@yahoo.com>
2020-11-27 09:21:32 +0200

    quran

---
+++ app/src/main/java/samir/com/quran/MainActivity.java
@@ -0,0 +1,14 @@
+package samir.com.quran;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
